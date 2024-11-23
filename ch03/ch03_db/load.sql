176b0259d9ae2a54cbf524602e35789130765fb5
Bharath Kumar <86688349+whoisbe@users.noreply.github.com>
2024-11-23 13:22:37 -0500

    Update load.sql (#3)
    fix path for parquet files

--- ch03/ch03_db/load.sql
+++ ch03/ch03_db/load.sql
@@ -1,3 +1,3 @@
-COPY systems FROM 'ch05_db/systems.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
-COPY prices FROM 'ch05_db/prices.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
-COPY readings FROM 'ch05_db/readings.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
+COPY systems FROM 'ch03_db/systems.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
+COPY prices FROM 'ch03_db/prices.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
+COPY readings FROM 'ch03_db/readings.parquet' (FORMAT 'parquet', COMPRESSION 'ZSTD');
